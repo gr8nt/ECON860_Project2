88d3610cecc541d91eb60b749e6b7db77ac7dbaf
gr8nt <143210366+gr8nt@users.noreply.github.com>
2023-12-12 12:42:42 -0500

    Update run_kmc.py

--- run_kmc.py
+++ run_kmc.py
@@ -19,6 +19,8 @@ if not os.path.exists('plots'):
   os.mkdir('plots')
 
 pyplot.scatter(dataset[:,0],dataset[:,1])
+pyplot.xlabel("Trait 1")
+pyplot.ylabel("Trait 2")
 pyplot.savefig("plots/scatterplot.png")
 pyplot.close()
 
@@ -29,6 +31,8 @@ def run_kmeans(n, dataset):
   results = machine.predict(dataset)
   centroids = machine.cluster_centers_
   pyplot.scatter(dataset[:,0],dataset[:,1], c=results)
+  pyplot.xlabel("Trait 1")
+  pyplot.ylabel("Trait 2")
   pyplot.scatter(centroids[:,0], centroids[:, 1], c="red", marker="*", s=250)
   pyplot.savefig("plots/scatterplot_kmeans_" + str(n) + ".png")
   pyplot.close()
@@ -53,6 +57,8 @@ def run_kmedoids(n, dataset):
   results = machine.predict(dataset)
   centroids = machine.cluster_centers_
   pyplot.scatter(dataset[:,0],dataset[:,1], c=results)
+  pyplot.xlabel("Trait 1")
+  pyplot.ylabel("Trait 2")
   pyplot.scatter(centroids[:,0], centroids[:, 1], c="red", marker="*", s=250)
   pyplot.savefig("plots/scatterplot_kmedoids_" + str(n) + ".png")
   pyplot.close()
